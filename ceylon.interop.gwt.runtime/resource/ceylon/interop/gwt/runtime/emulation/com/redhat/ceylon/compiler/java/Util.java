6cbfdaff32e6b50b95336ca63717e34be3025a9d
davidfestal <david.festal@serli.com>
2016-12-15 14:32:50 +0100

    Emulate `com.redhat.ceylon.compiler.java.Util.throwableMessage()`

--- ceylon.interop.gwt.runtime/resource/ceylon/interop/gwt/runtime/emulation/com/redhat/ceylon/compiler/java/Util.java
+++ ceylon.interop.gwt.runtime/resource/ceylon/interop/gwt/runtime/emulation/com/redhat/ceylon/compiler/java/Util.java
@@ -6,4 +6,26 @@ public class Util {
             throw new AssertionError("null value returned from native call not assignable to Object");
         return t;
     }
+    
+    /** 
+     * Generates a default message for when a Throwable lacks a non-null 
+     * message of its own. This can only happen for non-Ceylon throwable types, 
+     * but due to type erasure it's not possible to know at a catch site which 
+     * whether you have a Ceylon throwable
+     */
+    public static String throwableMessage(java.lang.Throwable t) {
+        String message = t.getMessage();
+        if (message == null) {
+            java.lang.Throwable c = t.getCause();
+            if (c != null) {
+                message = c.getMessage();
+            } else {
+                message = "";
+            }
+        }
+        return message;
+    }
+    
+    public static void rethrow(final Throwable t) {
+    }
 }
